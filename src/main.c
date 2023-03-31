b3c233ad54d776f52afd1b75da6315331b509dc6
PepeGF <gran.pepe@gmail.com>
2023-03-31 18:32:04 +0200

    SHLVL actualizado al entrar si existe, si no existe, peta

--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@
 
 #include "minishell.h"
 
+void	ft_set_shlvl(char **env_dup);
+
 void sig_handler(int sig)
 {
 	if (sig == SIGINT)
@@ -33,6 +35,7 @@ int	main(int argc, char **argv, char **envp)
 		return (1);
 	envp = (char **)envp;
 	env_dup = ft_dup_matrix(envp);
+	ft_set_shlvl(env_dup);
 	cmd_list = hardcoded(env_dup);
 	// printf("******* VARIABES DE ENTORNO ANTES DE UNSET ********\n");
 	// printf("%p->direccion de meoria de env_dup\n", env_dup);
@@ -44,7 +47,7 @@ int	main(int argc, char **argv, char **envp)
 
 	// ft_print_matrix(((t_command *)(cmd_list->content))->cmd_splited);
 	// echo_builtin(((t_command *)(cmd_list->content))->cmd_splited, STDOUT_FILENO);
-	cd_builtin(&env_dup, ((t_command *)(cmd_list->content))->cmd_splited);
+	// cd_builtin(&env_dup, ((t_command *)(cmd_list->content))->cmd_splited);
 	ft_print_matrix(env_dup);
 	// export_builtin(&env_dup, cmd_list);
 	ft_free_matrix(env_dup);
@@ -52,3 +55,29 @@ int	main(int argc, char **argv, char **envp)
 	return (0);
 }
 
+void	ft_set_shlvl(char **env_dup)
+{
+	char	*shlvl;
+	char	*aux;
+	int		level;
+
+	aux = ft_get_value_env(env_dup, "SHLVL");
+	if (aux != NULL)
+	{
+		level = ft_atoi(aux);
+		if (level != 0)
+			level++;
+		else
+			level = 1;
+		shlvl = ft_itoa(level);
+		free(aux);
+		aux = ft_strjoin("SHLVL=", shlvl);
+		free(shlvl);
+		ft_replace_line_in_matrix(env_dup, aux);
+		free(aux);
+	}
+	else
+	{
+		return ;
+	}
+}
