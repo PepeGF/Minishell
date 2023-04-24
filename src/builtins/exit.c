8a0fabe9d7ca7b948efbbde6794d59a0275b8e4e
rontomano <rontomeromano@gmail.com>
2023-04-24 12:02:05 +0200

    Exit: numeric argument error is 2, not 1

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -69,7 +69,7 @@ int	ft_exit_error_no_number(char *str)
 	ft_putstr_fd(str, STDERR_FILENO);
 	ft_putendl_fd(": numeric argument required", STDERR_FILENO);
 	g_exit = 255;
-	exit(EXIT_FAILURE);
+	exit(2);
 }
 
 int	ft_exit_with_arg(char *str)
